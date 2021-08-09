2d9df8a56b2578afbd72b98a88acd4fb347c2fbd
Rei Kurti <rei.kurti57@gmail.com>
2021-08-09 11:41:45 +0000

    Update views.py

--- agent/views.py
+++ agent/views.py
@@ -72,27 +72,3 @@ class UserRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     serializer_class = UserReadSerializer
     permission_classes = [AllowAny]
 
-class LoginView(APIView):
-    def post(self,request):
-        username=request.data['username']
-        password=request.data['password']
-
-        user=User.objects.filter(username=username).first()
-
-        if user is None:
-            raise AuthenticationFailed('User not found')
-
-        if not user.check_password(password):
-            raise AuthenticationFailed('Incorrect password')
-
-        payload={
-            'id':user.id,
-            'exp':datetime.datetime.utcnow()+datetime.timedelta(minutes=60),
-            'iat':datetime.datetime.utcnow()
-        }
-
-        token = jwt.encode(payload,'secret',algorithm='HS256').decode('utf-8')
-
-        return Response({
-            'jwt':token
-        })
